8cdbd8c973276277e6d6903735a39f9a253a1708
jayaprasad356 <jayaprasad356@gmail.com>
2022-11-18 03:27:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dpboss/Activities/SplashScreenActivity.java
@@ -0,0 +1,57 @@
+package com.example.dpboss.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.dpboss.R;
+import com.example.dpboss.helper.Session;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    Handler handler;
+    Session session;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        handler = new Handler();
+        GotoActivity();
+    }
+
+
+    private void GotoActivity()
+    {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+
+                Intent intent=new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+
+//                Session session = new Session(SplashActivity.this);
+//                if (session.getBoolean("is_logged_in")){
+//                    Intent intent=new Intent(SplashActivity.this, MainActivity.class);
+//                    startActivity(intent);
+//                    finish();
+//
+//                }else{
+//                    Intent intent=new Intent(SplashActivity.this,LoginActivity.class);
+//                    startActivity(intent);
+//                    finish();
+//
+//                }
+
+
+
+            }
+        },2000);
+    }
+}
---
+++ app/src/main/java/com/example/dpboss/Adapter/InfoAdapter.java
@@ -0,0 +1,65 @@
+package com.example.dpboss.Adapter;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.dpboss.Model.info_model;
+import com.example.dpboss.R;
+
+import java.util.ArrayList;
+
+
+public class InfoAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    final Activity activity;
+    final ArrayList<info_model> info_models;
+
+    public InfoAdapter(Activity activity, ArrayList<info_model> info_models) {
+        this.activity = activity;
+        this.info_models = info_models;
+    }
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(activity).inflate(R.layout.info, parent, false);
+        return new ItemHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holderParent, int position) {
+        final ItemHolder holder = (ItemHolder) holderParent;
+        final info_model info_model = info_models.get(position);
+
+        holder.tvDescription.setText(info_model.getDescription());
+        holder.tvTitle.setText(info_model.getTitle());
+
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return info_models.size();
+    }
+
+    static class ItemHolder extends RecyclerView.ViewHolder {
+
+        final TextView tvTitle,tvDescription;
+        public ItemHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvDescription = itemView.findViewById(R.id.tvDescription);
+
+
+
+
+        }
+    }
+}
+
